5d2bc000690486d8fb8efbc06306756c79f45822
Cristian Eigel <eigelc@gmail.com>
2014-09-05 13:34:22 +0200

    setup bootstrap and serving from js and css

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 from flask import Flask, url_for, redirect, request
 import sys
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
 
 if __name__ == '__main__':
     env = "dev"
