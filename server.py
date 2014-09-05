3c0cdd663a39c87d9552b3a824944d00a5db5056
Cristian Eigel <eigelc@gmail.com>
2014-09-05 13:24:14 +0200

    Hello world setup

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, request
+import sys
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return redirect(url_for('public/index.html'))
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
-if __name__ == "__main__":
-    app.run()
-
+if __name__ == '__main__':
+    env = "dev"
+    if len(sys.argv) > 1:
+        env = sys.argv[1]
+    if env == "dev":
+        app.run(host='0.0.0.0', port=3000, debug = True)
+    else:
+        app.run(host='0.0.0.0', port=80)
