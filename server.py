517abac1994a97354e11237bcd92cbaabb21dcb5
Cristian Eigel <eigelc@gmail.com>
2014-09-05 14:18:53 +0200

    Setup playlist route

--- server.py
+++ server.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def root():
     return app.send_static_file('index.html')
 
+@app.route('/playlist/<username>', methods=['GET'])
+def playlist(username):
+    return "Playlist for user %s" % username
+
 @app.route('/<path:path>')
 def static_proxy(path):
     # send_static_file will guess the correct MIME type
